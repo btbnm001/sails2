17efbc3d2aa8050f34505da2ad1a42759034d8f3
btbnm001 <42427528+btbnm001@users.noreply.github.com>
2018-08-31 10:48:58 +0700

    upd

--- config/routes.js
+++ config/routes.js
@@ -68,7 +68,7 @@ module.exports.routes = {
 'post /user/upd'			    : 'UserController.upd',  
 'post /user/del'			    : 'UserController.del',
 
-'/user/login'             : 'UserController.login',
+'post /user/login'         : 'UserController.login',
 '/user/logout'            : 'UserController.logout',
 
 };
